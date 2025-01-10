da9d12e90a0d7efb3618ec935164b8d195e3167a
0xSatoriSync <137118907+0xSatoriSync@users.noreply.github.com>
2025-01-10 00:18:34 -0500

    Update program address

--- src/programs/turbin3_prereq.rs
+++ src/programs/turbin3_prereq.rs
@@ -7,7 +7,7 @@ idlgen!
         "name": "turbin3_prereq",
         "metadata": 
         {
-            "address": "HC2oqz2p6DEWfrahenqdq2moUcga9c9biqRBcdK3XKU1"
+            "address": "ADcaide4vBtKuyZQqdU689YqEGZMCmS4tL35bdTv9wJa"
         },
         "instructions": 
         [
